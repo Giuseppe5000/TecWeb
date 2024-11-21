25c117ac576ec10307461395bfa0975a33fe64c0
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-11-21 17:09:35 +0100

    Inserimento di alcuni dati nel db #3

--- DB/db.sql
+++ DB/db.sql
@@ -79,3 +79,63 @@ CREATE TABLE commento (
        FOREIGN KEY (opera) REFERENCES opera(id) ON DELETE CASCADE,
        FOREIGN KEY (timerisp, utenterisp) REFERENCES commento(timestamp, utente) ON DELETE CASCADE
 );
+
+-- Inserimento Utenti
+INSERT INTO utente VALUES
+('efraine0', 'zV1<Y~TY,qOA', 'mbolf0@nyu.edu', false),
+('njobke1', 'zQ3<)fRz)xn+LG''', 'vchristofides1@dmoz.org', true),
+('delegood2', 'bN1)5s%nQoXnK+M', 'gelliker2@addtoany.com', true),
+('achaundy3', 'qY4\L9M.WTd20', 'mbeelby3@blogspot.com', false),
+('jhadlington4', 'wX4`0a@#\)>G~/', 'glabusch4@twitter.com', true),
+('nferreli5', 'zR1/=7xvtq@sE$gc', 'tbaudains5@nature.com', true),
+('dnarey6', 'oK7#}ZFi49x/', 'sscheu6@forbes.com', false),
+('sroft7', 'iY1/OJjo.FRF`%', 'xcurrer7@psu.edu', false),
+('hmilkins8', 'sN5{3AnWEVlK_W', 'xgiorgio8@wordpress.org', false),
+('akelleher9', 'jY2(PN$P&O0/RI', 'ldunton9@wisc.edu', true),
+('bburgisa', 'lJ9''ukEWnbo~0E', 'mhatza@amazon.co.uk', false),
+('tpantlingb', 'bX5<>38lMG', 'kbullanb@npr.org', false),
+('mabbesc', 'iJ5%qwB@Je', 'bgogginsc@miibeian.gov.cn', false),
+('wduffetd', 'sG7}5I/94F', 'asineyd@timesonline.co.uk', false),
+('kjurriese', 'fY5?yBcud', 'ybiddlese@amazon.co.uk', false),
+('cmccourtf', 'pM3$QD7)SkSVtGk.', 'cjaggarf@japanpost.jp', false),
+('wgavang', 'zW8*h%q8~h`ZK+''', 'tstofflerg@cdbaby.com', true),
+('ypatilloh', 'nI3!{9fa5', 'swhifeh@1688.com', false),
+('ceshelbyi', 'hG1.JUXw', 'fesmeadi@webeden.co.uk', false),
+('icharplingj', 'jK1_~Bv,l7', 'kslotj@woothemes.com', true);
+
+-- Inserimento Categorie
+INSERT INTO categoria VALUES
+('CAT1', 'DESC'),
+('CAT2', 'DESC'),
+('CAT3', 'DESC'),
+('CAT4', 'DESC'),
+('CAT5', 'DESC');
+
+-- Inserimento Iscrizioni
+INSERT INTO iscrizione VALUES
+('icharplingj', 'CAT1'),
+('ceshelbyi', 'CAT2'),
+('ceshelbyi', 'CAT4'),
+('wgavang', 'CAT3');
+
+-- Inserimento Opere
+INSERT INTO opera VALUES
+(1, './assets/opera1', 'Nome', 'DESC', 30.4, NULL);
+
+-- Inserimento Recensioni
+INSERT INTO recensione VALUES
+('wgavang', 1, 1),
+('icharplingj', 1, 4);
+
+-- Inserimento Acquisti
+INSERT INTO acquisto VALUES
+('icharplingj', 1, 50.10, '2024-03-22 12:50:05');
+
+-- Inserimento Appartenenze
+INSERT INTO appartenenza VALUES
+('CAT1', 1),
+('CAT2', 1);
+
+-- Inserimento Commenti
+INSERT INTO commento VALUES
+('2024-03-22 12:50:05', 'icharplingj', 'Bello', 1, NULL, NULL);
