33173311b159198cfaa31462245d0c875126f103
Giuseppe Tutino <giuseppetutino1@gmail.com>
2025-01-10 10:51:39 +0100

    Paginazione recensioni singolo nft #88

--- src/singolo-nft.php
+++ src/singolo-nft.php
@@ -3,6 +3,59 @@ session_start();
 
 require_once "./php/Database.php";
 require_once "./php/Navbar.php";
+require_once "./php/utils.php";
+
+function mostraRecensioni($recensioni, $pageNumber, $pageSize) {
+    $recensioni_html = "";
+    if(count($recensioni)>0){
+        $previousPages = $pageNumber*$pageSize;
+        for ($i = $previousPages; $i < $previousPages + $pageSize && $i < count($recensioni); $i++) {
+            $recensione = $recensioni[$i];
+            $voto = $recensione["voto"];
+            $commento = $recensione["commento"];
+            $utente = $recensione["utente"];
+            $date = strtotime($recensione["timestamp"]);
+            $date = date('d-m-Y',$date);
+            $recensioni_html.='<div class="comment">';
+            $recensioni_html.='<div class="head-comment">';
+            $recensioni_html.='<div class="user-comment">';
+            $recensioni_html.='<img class="logo_utente" src="assets/user.svg" alt="Logo profilo utente"/>';
+            $recensioni_html.='<span>'.$utente.'</span>';
+            $recensioni_html.='</div>';
+            $recensioni_html .= '<div>' . str_repeat('<span>&#9733;</span>', $voto) . '</div>';
+            $recensioni_html.= "<div>{$date}</div>";
+            $recensioni_html.='</div>';
+            $recensioni_html.='<p>'.$commento.'</p>';
+            $recensioni_html.='</div>';
+        }
+        return $recensioni_html;
+    }
+}
+
+function mostraAggiungiRecensione(&$aggiungi_recensione_html, $id) {
+    $aggiungi_recensione_html.='<form id="agg-recensione" class="user-form" action="singolo-nft.php" method="post">';
+    $aggiungi_recensione_html.='<fieldset>';
+    $aggiungi_recensione_html.='<legend>Aggiungi recensione</legend>';
+    $aggiungi_recensione_html.= '<fieldset id="stelle-recensione">';
+    $aggiungi_recensione_html.= '<legend>Dai un voto in stelle</legend>';
+    $aggiungi_recensione_html.= '<input type="radio" id="voto-1" name="voto" value="1" checked/>';
+    $aggiungi_recensione_html.= '<label class="star" for="voto-1">&#9733;</label>';
+    $aggiungi_recensione_html.= '<input type="radio" id="voto-2" name="voto" value="2"/>';
+    $aggiungi_recensione_html.= '<label class="star" for="voto-2">&#9733;</label>';
+    $aggiungi_recensione_html.= '<input type="radio" id="voto-3" name="voto" value="3"/>';
+    $aggiungi_recensione_html.= '<label class="star" for="voto-3">&#9733;</label>';
+    $aggiungi_recensione_html.= '<input type="radio" id="voto-4" name="voto" value="4"/>';
+    $aggiungi_recensione_html.= '<label class="star" for="voto-4">&#9733;</label>';
+    $aggiungi_recensione_html.= '<input type="radio" id="voto-5" name="voto" value="5"/>';
+    $aggiungi_recensione_html.= '<label class="star" for="voto-5">&#9733;</label>';
+    $aggiungi_recensione_html.= '</fieldset>';
+    $aggiungi_recensione_html.='<label for="recensione">Recensione:</label>';
+    $aggiungi_recensione_html.='<textarea id="recensione" name="recensione" required></textarea>';
+    $aggiungi_recensione_html.='<input type="hidden" name="id" value="'.$id.'"/>';
+    $aggiungi_recensione_html.='<input type="submit" value="Aggiungi" class="button" name="aggiungi"></input>';
+    $aggiungi_recensione_html.='</fieldset>';
+    $aggiungi_recensione_html.='</form>';
+}
 
 $paginaHTML = file_get_contents('./static/singolo-nft.html');
 $database = new Database();
@@ -16,7 +69,11 @@ $recensioni_html='';
 $aggiungi_recensione_html='';
 $acquisto_res='';
 
-
+$pageSize = 4;
+$pageNumber = 0;
+if (isset($_GET['page']))
+    $pageNumber = intval($_GET['page']);
+$recensioniDaMostrare = 0;
 
 if (!$connessioneOK) {
 
@@ -27,9 +84,7 @@ if (!$connessioneOK) {
 
     #Se aggiunge una recensione
     if (isset($_POST['aggiungi']) && isset($_SESSION['username'])) {
-        
         $username=$_SESSION['username'];
-        //recupera valori del form
         $id=$_POST['id'];
         $voto=$database->pulisciInput($_POST['voto']);
         $recensione=$database->pulisciInput($_POST['recensione']);
@@ -79,10 +134,6 @@ if (!$connessioneOK) {
     $query='SELECT * FROM opera WHERE id='.$id;
     $opera=$database->executeQuery($query);
 
-    $query='SELECT * FROM recensione WHERE opera="' . $id . '" ORDER BY timestamp DESC';
-    $recensioni=$database->executeQuery($query);
-    $database->closeConnection();
-
     if(count($opera)==1){
         $nome_opera=$opera[0]["nome"];
         
@@ -121,57 +172,42 @@ if (!$connessioneOK) {
         }
     }
 
-    if(count($recensioni)>0){
-        #inserisco le recensioni
-        foreach($recensioni as $recensione){
-            $date = strtotime($recensione["timestamp"]);
-            $date = date('d-m-Y',$date);
-            $recensioni_html.='<div class="comment">';
-            $recensioni_html.='<div class="head-comment">';
-            $recensioni_html.='<div class="user-comment">';
-            $recensioni_html.='<img class="logo_utente" src="assets/user.svg" alt="Logo profilo utente"/>';
-            $recensioni_html.='<span>'.$recensione["utente"].'</span>';
-            $recensioni_html.='</div>';
-            $recensioni_html .= '<div>' . str_repeat('<span>&#9733;</span>', $recensione["voto"]) . '</div>';
-            $recensioni_html.= "<div>{$date}</div>";
-            $recensioni_html.='</div>';
-            $recensioni_html.='<p>'.$recensione["commento"].'</p>';
-            $recensioni_html.='</div>';
-        }
-    }
+    $query='SELECT * FROM recensione WHERE opera="' . $id . '" ORDER BY timestamp DESC';
+    $recensioni=$database->executeQuery($query);
+    $database->closeConnection();
+    $recensioni_html = mostraRecensioni($recensioni, $pageNumber, $pageSize);
+    $recensioniDaMostrare = count($recensioni) - $pageNumber*$pageSize - $pageSize;
+}
 
-    #SE LOGGATO
-    if(isset($_SESSION['username'])){
-        $aggiungi_recensione_html.='<form id="agg-recensione" class="user-form" action="singolo-nft.php" method="post">';
-        $aggiungi_recensione_html.='<fieldset>';
-        $aggiungi_recensione_html.='<legend>Aggiungi recensione</legend>';
-        $aggiungi_recensione_html.= '<fieldset id="stelle-recensione">';
-        $aggiungi_recensione_html.= '<legend>Dai un voto in stelle</legend>';
-        $aggiungi_recensione_html.= '<input type="radio" id="voto-1" name="voto" value="1" checked/>';
-        $aggiungi_recensione_html.= '<label class="star" for="voto-1">&#9733;</label>';
-        $aggiungi_recensione_html.= '<input type="radio" id="voto-2" name="voto" value="2"/>';
-        $aggiungi_recensione_html.= '<label class="star" for="voto-2">&#9733;</label>';
-        $aggiungi_recensione_html.= '<input type="radio" id="voto-3" name="voto" value="3"/>';
-        $aggiungi_recensione_html.= '<label class="star" for="voto-3">&#9733;</label>';
-        $aggiungi_recensione_html.= '<input type="radio" id="voto-4" name="voto" value="4"/>';
-        $aggiungi_recensione_html.= '<label class="star" for="voto-4">&#9733;</label>';
-        $aggiungi_recensione_html.= '<input type="radio" id="voto-5" name="voto" value="5"/>';
-        $aggiungi_recensione_html.= '<label class="star" for="voto-5">&#9733;</label>';
-        $aggiungi_recensione_html.= '</fieldset>';
-        $aggiungi_recensione_html.='<label for="recensione">Recensione:</label>';
-        $aggiungi_recensione_html.='<textarea id="recensione" name="recensione" required></textarea>';
-        $aggiungi_recensione_html.='<input type="hidden" name="id" value="'.$id.'"/>';
-        $aggiungi_recensione_html.='<input type="submit" value="Aggiungi" class="button" name="aggiungi"></input>';
-        $aggiungi_recensione_html.='</fieldset>';
-        $aggiungi_recensione_html.='</form>';
-    }else{
-        $aggiungi_recensione_html.='<p class="center">Se vuoi aggiungere una recensione <a href="accedi.php">Accedi</a> al tuo profilo</p>';
-    }
+#SE LOGGATO
+if(isset($_SESSION['username'])){
+    mostraAggiungiRecensione($aggiungi_recensione_html, $id);
+}else{
+    $aggiungi_recensione_html.='<p class="center">Se vuoi aggiungere una recensione <a href="accedi.php">Accedi</a> al tuo profilo</p>';
+}
 
+$linkPaginaPrecedente = "";
+if ($pageNumber > 0) {
+    $prevPageNumber = $pageNumber - 1;
+    $queryString = generatePageNumber($prevPageNumber);
+    if(isset($_GET['id']))
+        $linkPaginaPrecedente =  "<a class='prev-page' href=\"singolo-nft.php?{$queryString}\">&#10094;</a>";
+    else
+        $linkPaginaPrecedente =  "<a class='prev-page' href=\"singolo-nft.php?id={$id}{$queryString}\">&#10094;</a>";
+}
+
+$linkPaginaSuccessiva = "";
+if ($recensioniDaMostrare > 0) {
+    $nextPageNumber = $pageNumber + 1;
+    $queryString = generatePageNumber($nextPageNumber);
+    if(isset($_GET['id']))
+        $linkPaginaSuccessiva = "<a class='next-page' href=\"singolo-nft.php?{$queryString}\">&#10095;</a>";
+    else
+        $linkPaginaSuccessiva = "<a class='next-page' href=\"singolo-nft.php?id={$id}{$queryString}\">&#10095;</a>";
 }
 
 $navbar = new Navbar("");
 
-$find=['{{OPERA}}','{{DESCRIZIONE}}','{{RECENSIONI}}','{{AGGIUNGI_RECENSIONE}}','{{ACQUISTO_RES}}', '{{NAVBAR}}','{{NOME_NFT}}'];
-$replacement=[$opera_html,$descr_html,$recensioni_html,$aggiungi_recensione_html,$acquisto_res, $navbar->getnavbar(),$nome_opera];
+$find=['{{OPERA}}','{{DESCRIZIONE}}','{{RECENSIONI}}','{{AGGIUNGI_RECENSIONE}}','{{ACQUISTO_RES}}', '{{NAVBAR}}','{{NOME_NFT}}', '{{PAGINA_PRECEDENTE}}', '{{PAGINA_SUCCESSIVA}}', '{{PAGINA_CORRENTE}}'];
+$replacement=[$opera_html,$descr_html,$recensioni_html,$aggiungi_recensione_html,$acquisto_res, $navbar->getnavbar(),$nome_opera,$linkPaginaPrecedente, $linkPaginaSuccessiva,"<span class='page-number'>Pagina: {$pageNumber}</span>"];
 echo str_replace($find, $replacement, $paginaHTML);
