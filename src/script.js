d7e56f06bcab28438df6e6e7b688f49e8c3d92a7
Giuseppe Tutino <giuseppetutino1@gmail.com>
2025-01-14 10:25:16 +0100

    Aggiunti controlli lato client su descrizione di carica nft #91

--- src/script.js
+++ src/script.js
@@ -23,3 +23,37 @@ function slider(){
         cCont.scrollBy(200,0);
     });
 }
+
+// ===== CONTROLLI FORM ======
+function validazioneCaricaNft() {
+    const input = document.getElementById("descrizione");
+
+    let node;
+    let p = input.parentNode.previousSibling;
+    if (p.className == "messaggi-form") {
+        p.remove();
+        p = input.parentNode.previousSibling;
+    }
+
+    let desc = input.value;
+    if (desc.includes(".")) {
+        let desc = desc.split(".");
+        let alt = desc[0];
+        if (alt.length > 100) {
+            node = document.createElement("p");
+            node.className = "messaggi-form";
+            node.appendChild(document.createTextNode("La prima frase del campo descrizione (dall'inizio fino al primo punto) deve essere lunga al massimo 150 caratteri!"));
+            p.after(node);
+            return false;
+        }
+    }
+    else {
+        node = document.createElement("p");
+        node.className = "messaggi-form";
+        node.appendChild(document.createTextNode("Il campo descrizione deve avere almeno un punto alla fine!"));
+        p.after(node);
+        return false;
+    }
+
+
+}
