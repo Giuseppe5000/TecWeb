fad84d55c1c7c48b9ff53cd972d71bf5db32f6e7
Giuseppe Tutino <giuseppetutino1@gmail.com>
2025-01-16 17:51:46 +0100

    Rimossa funzione slider del carosello

--- src/script.js
+++ src/script.js
@@ -10,20 +10,6 @@ if (filterButton) {
     }
 }
 
-function slider(){
-    const prev = document.querySelector(".prev");
-    const next = document.querySelector(".next");
-    const cCont = document.querySelector(".slideshow");
-
-    prev.addEventListener("click",()=>{
-        cCont.scrollBy(-200,0);
-    });
-
-    next.addEventListener("click",()=>{
-        cCont.scrollBy(200,0);
-    });
-}
-
 // ===== CONTROLLI FORM ======
 function validazioneCaricaNft() {
     const form = document.getElementById("add-nft");
