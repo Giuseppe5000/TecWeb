4099410b1163712da47015290cc0db46c9ac64d6
Sadixo <manuelgusella13@gmail.com>
2025-01-19 11:05:53 +0100

    Diviso file accedi.php dal post effettivo e aggiunti controlli #77

--- src/accedi.php
+++ src/accedi.php
@@ -1,58 +1,36 @@
 <?php
 
-require_once "./php/Database.php";
-require_once "./php/Utente.php";
 require_once "./php/Navbar.php";
 require_once "./php/utils.php";
 session_start();
-
 $messaggi = array("generico"=>"", "username"=>"", "password"=>"");
 $username = "";
 $password = "";
 
-function checkInput($username, $password, &$messaggi) {
-	if (strlen($username)==0)
-		$messaggi["username"] .= makeMessageParagraph('Il campo <span lang="en">username</span> non può essere vuoto!');
-    if (strlen($username)>30)
-        $messaggi["username"] .= makeMessageParagraph('Il campo <span lang="en">username</span> non può superare i 30 caratteri!');
-    if (preg_match("/[\W]/", $username))
-		$messaggi["username"] .= makeMessageParagraph('Il campo <span lang="en">username</span> può contenere solo lettere e numeri!');
-
-	if (strlen($password)==0)
-		$messaggi["password"] .= makeMessageParagraph('Il campo <span lang="en">password</span> non può essere vuoto!');
-    if (strlen($password)>30)
-        $messaggi["password"] .= makeMessageParagraph('Il campo <span lang="en">password</span> non può superare i 30 caratteri!');
-    if (!preg_match("/^[a-zA-Z0-9!@#$]*$/", $password))
-		$messaggi["password"] .= makeMessageParagraph('Il campo <span lang="en">password</span> può contenere solo lettere, numeri e i seguenti caratteri speciali: ! @ # $');
-
-    return strlen($messaggi["username"])==0 && strlen($messaggi["password"])==0;
+if(isset($_SESSION['username'])){
+    header('Location: profilo.php');
+    exit;
 }
 
-if(isset($_POST['submit'])){
-    $database = new Database();
-    $username = $database->pulisciInput($_POST['username']);
-	$password = $database->pulisciInput($_POST['password']);
-
-    if(checkInput($username, $password, $messaggi)){
-		$connessioneOK = $database->openConnection();
-
-		if(!$connessioneOK){
-			$utente = new Utente($database->getConnection(), $username, $password, "");
-			$loginSuccessfull = $utente->login();
-			$database->closeConnection();
-
-			if( $loginSuccessfull ){
-				$_SESSION['username'] = $utente->getUsername();
-				header('Location: ./profilo.php');
-				exit;
-			}
-			else{
-				$messaggi["generico"] .= makeMessageParagraph('<span lang="en">Username</span> o <span lang="en">password</span> errati');
-			}
-		} else {
-            header('Location: ./500.php');
-		}
-	}
+if(isset($_SESSION['user'])){
+    $username=$_SESSION['user'];
+    unset($_SESSION['user']);
+}
+if(isset($_SESSION['password'])){
+    $password=$_SESSION['password'];
+    unset($_SESSION['password']);
+}
+if(isset($_SESSION['mexGenerico'])){
+    $messaggi["generico"]=$_SESSION['mexGenerico'];
+    unset($_SESSION['mexGenerico']);
+}
+if(isset($_SESSION['mexUsername'])){
+    $messaggi["username"]=$_SESSION['mexUsername'];
+    unset($_SESSION['mexUsername']);
+}
+if(isset($_SESSION['mexPassword'])){
+    $messaggi["password"]=$_SESSION['mexPassword'];
+    unset($_SESSION['mexPassword']);
 }
 
 $navbar = new Navbar("Accedi");
