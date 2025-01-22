2e2f7199877429c2b69018919d42ab4a329b764b
Perruzz <perozzoandrea3@gmail.com>
2025-01-22 11:12:08 +0100

    aggiunto uno spazio

--- src/profilo.php
+++ src/profilo.php
@@ -106,6 +106,7 @@ if(isset($_SESSION['username'])){
                 $recensioni_html .= '<input type="image" src="assets/edit_icon.svg" alt="modifica recensione ' . $count . '" name="modifica">';
                 $recensioni_html .= '</div>';
                 $recensioni_html .= '</form>';
+                
                 $recensioni_html .= '<form class="form_recensione" action="php/post/recensione/cancella-recensione.php" method="post">';
                 $recensioni_html .= '<div>';
                 $recensioni_html .= '<input type="hidden" name="currentPage" value="' . $_SERVER["PHP_SELF"] . '"/>';
